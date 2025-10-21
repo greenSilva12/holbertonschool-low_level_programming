686a022b28f9db7390101284b32eea3d6030cdcf
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-21 12:54:26 +0000

    printf1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main - the principal function
-printf - prints "with proper grammar, but the outcome is a piece of art,"
-Return: 0
+* main - the principal function
+* printf - prints "with proper grammar, but the outcome is a piece of art,"
+* Return: 0
 */
-int main (void)
+int main(void)
 {
-	printf (%s"ith proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
