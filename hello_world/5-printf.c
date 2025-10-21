e679bfb8656a4d581e8f646c068844262df048a4
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-21 12:18:02 +0000

    t5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main (void)
 {
-	printf("ith proper grammar, but the outcome is a piece of art,\n")
+	printf ("ith proper grammar, but the outcome is a piece of art,\n")
 	return (0)
 }
