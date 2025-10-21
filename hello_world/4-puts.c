524c3bdcc1f356c8579dcf2fca4668138e26d0e3
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-21 11:44:45 +0000

    t4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
-* puts - prints "programming"
+* main - the principal function
+* puts - prints "Programming is like building a multilingual puzzle"
+* Return: 0
 */
 int main(void)
 {
