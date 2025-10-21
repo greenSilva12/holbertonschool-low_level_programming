a5d1b5e77c3739f4f5cdfc8d434cb76068bc7f74
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-21 12:43:58 +0000

    printf1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,6 +3,7 @@
 *main - the principal function
 printf - prints "with proper grammar, but the outcome is a piece of art,"
 Return: 0
+*/
 int main (void)
 {
 	printf (%s"ith proper grammar, but the outcome is a piece of art,\n")
