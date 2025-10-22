fbe112a1e7ee2cf95a201bcf1694d73d139f87f5
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-22 13:04:15 +0000

    if

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,12 +17,12 @@ int main(void)
 {
 	printf("n:%d is positive\n", n);
 }
-	if (n == 0)
+	else if (n == 0)
 {
 	printf("n:%d is zero\n", n);
 }
-	if (n < 0)
-{
+	else
+
 	printf("n:%d is negative\n", n);
 }
 	return (0);
