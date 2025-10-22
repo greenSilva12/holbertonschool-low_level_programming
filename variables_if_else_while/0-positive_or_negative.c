9f321c3d2b03c4dd57e9c86f942fb10ac1c1b7e7
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-22 11:47:26 +0000

    if

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <temps.h>
 int main(void)
 {
 	int n;
-	printf("n: %d \n", n);
-
+	printf("n: %d \n", n) ;
 	if (n < 0)
 {
 	printf("n: is positive %d \n") ;
