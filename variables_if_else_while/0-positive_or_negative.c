b33241ec59b422c9b92001c4c703c7569258e1e1
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-22 09:05:08 +0000

    if else

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <temps.h>
+int main (){
+
+	int n ;
+	printf("n: %d\n", n)
+
+	if (n<0){
+	printf("n: %d\n", is positive );
+}
+	if (n=0){
+	printf("n: %d\n", is zero );
+}
+	if (n>0){
+	printf("n: %d\n", is negative );
+}
+	return (0);
+}
+
