5553a62fdf31145d2188b3cb75697b62b0f4c42f
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-27 13:15:50 +0000

    more functions

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* int - initialition
+* if - condition
+* return: 1
+*/
+int _isupper(int c)
+{
+    int c;
+    if (c >= 'A' && c <= 'Z')
+    {
+    printf("c: %d\n", c);
+    }
+    return (1);
+}
