9fdc955a3f1868ba202da9f4bc9a238d3cd53e20
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-27 22:03:04 +0000

    Fix all Betty warnings

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,12 +4,11 @@
  * _isdigit - checks if a character is a digit (0-9)
  * @c: The character to check
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: (1) if c is a digit, (0) otherwise
  */
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-	return (1);
-    else
-    return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
