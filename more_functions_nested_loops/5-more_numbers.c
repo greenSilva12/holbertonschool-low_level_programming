7ea77eb39499c287b19b2cb07a8ec6fd4d0f5715
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-28 09:26:00 +0000

    task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+ void more_numbers(void)
+ {
+    int i;
+
+	for (i = 0; i <= 14; i++)
+	_putchar(i + '0');
+	_putchar('\n');
+
+ }
