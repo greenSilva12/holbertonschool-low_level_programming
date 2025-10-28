24c0b33e2bf6c5e7e6ad897150d6ea5403c97137
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-28 10:02:52 +0000

    task test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,10 +7,11 @@
  */
  void more_numbers(void)
  {
-    int i;
+    int n;
 
-	for (i = 0; i = 14; i++)
-	_putchar(i + '0');
+	for (n == '0',n <= '14', n++)
+    {
+        _putchar(n);
+    }
 	_putchar('\n');
-
- }
+}
