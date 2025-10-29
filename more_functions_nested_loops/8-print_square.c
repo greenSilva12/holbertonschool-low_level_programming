d74ff064bb0ee1bc371aead1a4be040b5f31b2e2
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-29 08:47:40 +0000

    task8 tableau

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*
+*/
+void print_square(int size);
+{
+int i, n;
+if(i <= 0)
+{
+	_putchar('0\n');
+	return;
+}
+
+for (i = 0; i < n; i++)
+_putchar('#');
+
+_putchar('\n'); 
+}
+
