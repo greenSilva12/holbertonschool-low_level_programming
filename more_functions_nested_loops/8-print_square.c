16392ab7f6bc18da07c5fc264745e07438e99cdc
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-29 09:33:52 +0000

    task8 tableau

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
-*
+* print_square - faz um quadro
+* size - e o tamanho do quadro
+* Return: 0
 */
 void print_square(int size);
 {
-	int i, j;
+int i, j;
 
-	if (size <= 0)
-	{
-	_putchar('\n');
-	return;
-	}
-	
-	for (i = 0; i < n; i++)
-	{
-	for (j = 0; j < i; j++)
-		_putchar(' ');
-	
-	_putchar('\\');
-	_putchar('\n');
-	}
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+	_putchar(' ');
+
+_putchar('\\');
+_putchar('\n');
+}
 }
