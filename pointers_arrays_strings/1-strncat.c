e61b877c443333d9b333eb6ca4dfbf97ad35afc2
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-30 22:27:19 +0000

    task1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using at most n bytes from src
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of bytes to use from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	/* Find the end of dest */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 #define MAIN_H
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src)
 #endif
