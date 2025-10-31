6d63699e5505e28d116799f49443bd5f76091d11
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-31 13:48:31 +0000

    testar mudar dos valores

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* @a - variavel
+* @b - pointeur
+* @swap_int - funcao
+* Return: 0
+*/
+void swap_int(int *a, int *b);
+{
+    *a = 75 && *b = 100;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #define MAIN_H
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
