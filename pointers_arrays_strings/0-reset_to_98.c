41fe5dc2bafd915389faa5e56ffff6639f62e323
greenSilva12 <greenbaysilva12@gmail.com>
2025-10-31 08:38:49 +0000

    ver como funciona

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * @n - variavel
+ * @p - pointeur de i
+ * Return: 0 
+ */
+ void reset_to_98(int *n);
+ {
+    int i = 98;
+    int *p = &i;
+    printf("%d\n", *p)
+    return (0);
+ }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
+void reset_to_98(int *n);
+
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src)
 #endif
